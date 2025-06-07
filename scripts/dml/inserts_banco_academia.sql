201e085a23ee0233f0d950c051fbcc1a2cff1e1a
GuilhermeCarrer <guilhermercarrer@gmail.com>
2025-06-07 15:09:28 -0300

    insert tabela clientes

---
+++ scripts/dml/inserts_banco_academia.sql
@@ -0,0 +1,13 @@
+-- Tabela Clientes 
+INSERT INTO Clientes (nm_cliente, cpf, email, telefone, dt_nascimento, endereco, dt_cadastro)
+VALUES
+('Gabriel Fillipe Casagrande Fernandes', '11122233344', 'gabriel.fernandes@email.com', '48912345678', '20030510', 'Rua das Flores, 123, Criciúma/SC', '20230701'),
+('Guilherme Rabello Carrer', '22233344455', 'guilherme.carrer@email.com', '48923456789', '20040820', 'Av. Centenário, 456, Criciúma/SC', '20230705'),
+('Nathan Rocha Gomes', '33344455566', 'nathan.gomes@email.com', '48934567890', '20031201', 'Rua São Paulo, 789, Criciúma/SC', '20230710'),
+('Miguel Nilo Rosa', '44455566677', 'miguel.rosa@email.com', '48945678901', '20040715', 'Av. Santos Dumont, 101, Criciúma/SC', '20230715'),
+('Henrique da Silva Ronzani', '55566677788', 'henrique.ronzani@email.com', '48956789012', '20030322', 'Rua Cel. Pedro Ferreira, 202, Criciúma/SC', '20230720'),
+('Luis Filipe Damiani Colombo', '66677788899', 'luis.colombo@email.com', '48967890123', '20041111', 'Rua Benjamin Constant, 303, Criciúma/SC', '20230801'),
+('Fernando Fernandes de Farias', '77788899900', 'fernando.farias@email.com', '48978901234', '20030930', 'Rua José Bonifácio, 404, Criciúma/SC', '20230805'),
+('Thiago de Moliner Colombo', '88899900011', 'thiago.colombo@email.com', '48989012345', '20040425', 'Av. Getúlio Vargas, 505, Criciúma/SC', '20230810'),
+('Kauan Custodio Propodoski', '99900011122', 'kauan.propodoski@email.com', '48990123456', '20030610', 'Rua Coronel Pedro Benedet, 606, Criciúma/SC', '20230815'),
+('Leonardo da Silva Joaquim', '00011122233', 'leonardo.joaquim@email.com', '48901234567', '20040105', 'Rua Felipe Schmidt, 707, Criciúma/SC', '20230820');
