53d2904b9f83a93cfce81a0035f539e1686577fb
FernandoFernandesFarias <fariasfernando028@gmail.com>
2025-06-10 19:10:36 -0300

    Excluindo arquivos

--- CRUD/View/Pages/index.php
+++ CRUD/View/Pages/index.php
@@ -2,36 +2,15 @@
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/CRUD/View/Styles/login.css">
-  <link rel="stylesheet" href="/CRUD/View/Styles/style.css">
+  <link rel="stylesheet" href="/CRUD/View/Styles/home.css">
 </head>
 <body>
+  
 
-  <div class="container d-flex justify-content-center align-items-center min-vh-100">
-    <div class="login-container">
-      <h3 class="text-center mb-4">Login</h3>
-      <form>
-        <div class="mb-3">
-          <label for="email" class="form-label">E-mail</label>
-          <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail" required>
-        </div>
-        <div class="mb-3">
-          <label for="senha" class="form-label">Senha</label>
-          <input type="password" class="form-control" id="senha" placeholder="Digite sua senha" required>
-        </div>
-        <div class="d-grid">
-          <a href="/CRUD/View/Pages/Home.html" type="submit" class="btn btn-primary">Entrar</a>
-        </div>
-        <div class="mt-3 text-center">
-          <a href="cadastro.html" target="_blank">Não tem login? Cadastre-se</a>
-        </div>
-      </form>
-    </div>
-  </div>
-
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
